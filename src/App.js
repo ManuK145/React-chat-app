f865b3565a96d7ede8d4e479e0ee2b032adf1258
ManuKaur145 <zhac080@live.rhul.ac.uk>
2023-03-14 16:25:51 +0000

    Imported google authentication and react hooks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import { auth } from './firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
 
 const style = {
   appContainer: `max-w-[728px] mx-auto text-center`,

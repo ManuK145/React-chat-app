6a9cc182d96a65f670a7ffcce69a3a6ed3d5fd89
ManuKaur145 <zhac080@live.rhul.ac.uk>
2023-03-14 15:50:52 +0000

    Added variabled for styling using tailwind

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
 
+const style = {
+  appContainer: `max-w-[728px] mx-auto text-center`,
+  sectionContainer: `flex flex-col h-[90vh] bg-gray-100 mt-10 shadow-xl border relative`,
+};
+
 function App() {
   return (
-    <div>
+    <div className={style.appContainer}>
+      <section className={style.sectionContainer}>
+
+      </section>
           
     </div>
   );

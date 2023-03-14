1c3f5df6a376a45050ce33bd3802e5102c7cc98b
ManuKaur145 <zhac080@live.rhul.ac.uk>
2023-03-14 19:21:43 +0000

    Added styling of 'name' using css

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
+const style = {
+    message: `flex items-center shadow-xl m-4 py-2 px-3 rounded-tl-full rounded-tr-full`,
+    name: `absolute mt-[-4rem] text-gray-600 text-xs`,
+};
+
 const Message = () => {
   return (
     <div>
-      
+        <div className={style.message}>
+            <p className={style.name}>Manu</p>
+            <p>I am learning react.</p>
+        </div>
     </div>
   )
 }

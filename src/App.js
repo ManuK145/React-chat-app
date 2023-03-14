1ba52eb97a8f27ed7d80739dccdde40cea5c63ef
ManuKaur145 <zhac080@live.rhul.ac.uk>
2023-03-14 15:54:00 +0000

    Imported navbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Navbar from "./components/Navbar";
 
 const style = {
   appContainer: `max-w-[728px] mx-auto text-center`,
@@ -9,7 +10,7 @@ function App() {
   return (
     <div className={style.appContainer}>
       <section className={style.sectionContainer}>
-
+        <Navbar />
       </section>
           
     </div>

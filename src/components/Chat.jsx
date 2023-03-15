a34ac10c8a3c03afc5bc81e2d77f6a2f8c3d6ed5
ManuKaur145 <zhac080@live.rhul.ac.uk>
2023-03-15 12:32:20 +0000

    Added method for showing messages to left and right appropriately

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,4 +1,9 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react';
+import Message from './Message';
+import { db } from '../firebase';
+import { query, collection, orderBy, onSnapshot } from 'firebase/firestore';
+import SendMessage from './SendMessage';
+
 
 const style = {
     main: `flex flex-col p-[10px]`,
@@ -6,10 +11,33 @@ const style = {
   
 
 const Chat = () => {
+  const [messages, setMessages] = useState([]);
+  const scroll = useRef();
+
+  useEffect(() => {
+    const q = query(collection(db, 'messages'), orderBy('timestamp'));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      let messages = [];
+      querySnapshot.forEach((doc) => {
+        messages.push({ ...doc.data(), id: doc.id });
+      });
+      setMessages(messages);
+    });
+    return () => unsubscribe();
+  }, []);
+
   return (
-    <main className={style.main}>
-        
-    </main>
+    <>
+        <main className={style.main}>
+        {messages &&
+          messages.map((message) => (
+            <Message key={message.id} message={message} />
+          ))}
+        </main>
+
+        <SendMessage scroll={scroll} />
+        <span ref={scroll}></span>
+    </>
   )
 }
 
